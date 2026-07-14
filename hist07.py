9552d7ee4c34cadfc576a8c7036f9db7dbdad6b8
tengzhang <tengzhang@gmail.com>
2026-07-14 11:50:34 +0800

    parserutils.py created

--- hist07.py
+++ hist07.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import argparse
 import sys
 import pathlib
+import parserutils
+import argparse
 
 def norm(options=None):
     vals = np.random.normal(options.loc, options.scale, options.size)
@@ -16,38 +17,6 @@ def norm(options=None):
     fig.savefig(filename)
     print(f'Figure saved as {filename}')
 
-def type_float(val):
-    try:
-        return float(val)
-    except (ValueError, TypeError):
-        raise argparse.ArgumentTypeError(f'invalid float value: {val}')
-
-def type_int(val):
-    try:
-        return int(val)
-    except (ValueError, TypeError):
-        raise argparse.ArgumentTypeError(f'invalid float value: {val}')
-
-
-def type_positive_float(val):
-    val = type_float(val)
-    if val > 0:
-        return val
-    raise argparse.ArgumentTypeError(f'invalid positive number: {val}')
-
-def type_positive_int(val):
-    val = type_int(val)
-    if val > 0:
-        return val
-    raise argparse.ArgumentTypeError(f'invalid a positive integer: {val}')
-
-def type_seed(val):
-    val = type_int(val)
-    if 0 <= val < 2**32:
-        np.random.seed(val)
-        return val
-    raise argparse.ArgumentTypeError(f'not in [0, 2**32): {val}')
-
 
 def get_parser(file):
     parser = argparse.ArgumentParser(prog='normal distribution',
@@ -58,12 +27,12 @@ def get_parser(file):
                         help='Mean of the distribution')
     parser.add_argument('-scale',
                         metavar='FLOAT',
-                        type=type_positive_float,
+                        type=parserutils.type_positive_float,
                         default=1.5,
                         help='Width of the distribution')
     parser.add_argument('-size',
                         metavar='INT',
-                        type=type_positive_int,
+                        type=parserutils.type_positive_int,
                         default=200,
                         help='Width of the distribution')
     parser.add_argument('-INTERACTIVE',
@@ -71,7 +40,7 @@ def get_parser(file):
                         help='Enable interactive mode')
     parser.add_argument('-seed',
                         metavar='INT',
-                        type=type_seed,
+                        type=parserutils.type_seed,
                         help='The integer to set random state')
     parser.add_argument('-JOBNAME',
                         default=pathlib.Path(file).stem,
---
+++ parserutils.py
@@ -0,0 +1,33 @@
+import argparse
+import numpy as np
+
+def type_float(val):
+    try:
+        return float(val)
+    except (ValueError, TypeError):
+        raise argparse.ArgumentTypeError(f'invalid float value: {val}')
+
+def type_int(val):
+    try:
+        return int(val)
+    except (ValueError, TypeError):
+        raise argparse.ArgumentTypeError(f'invalid float value: {val}')
+
+def type_positive_float(val):
+    val = type_float(val)
+    if val > 0:
+        return val
+    raise argparse.ArgumentTypeError(f'invalid positive number: {val}')
+
+def type_positive_int(val):
+    val = type_int(val)
+    if val > 0:
+        return val
+    raise argparse.ArgumentTypeError(f'invalid a positive integer: {val}')
+
+def type_seed(val):
+    val = type_int(val)
+    if 0 <= val < 2**32:
+        np.random.seed(val)
+        return val
+    raise argparse.ArgumentTypeError(f'not in [0, 2**32): {val}')
