6dbff22791dae291acbaadba71fbc30189c30ca7
tengzhang <tengzhang@gmail.com>
2026-07-16 09:17:24 +0800

    norm as regular package

--- module/norm/ourmodule.py
+++ module/norm/ourmodule.py
@@ -1,5 +1,11 @@
+import sys
+
 def square(x):
     print(x**2)
 
 def hello(x):
     print(f'Hello, {x}!')
+
+if __name__ == '__main__':
+    hello(f'installing {sys.argv[1] if len(sys.argv) > 1 else None}..')
+
